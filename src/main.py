1302b2d9be3d94e8aaa400ef89cf0d440e4803ce
David Mutchler <mutchler@rose-hulman.edu>
2024-07-24 15:55:47 -0400

    Small style changes.  Nothing of consequence.

--- src/main.py
+++ src/main.py
@@ -11,14 +11,15 @@ def main():
     screen = pygame.display.set_mode((640, 650))  # TODO: Choose your own size
     pygame.display.set_caption("My Project")  # TODO: Choose your own title
     clock = pygame.time.Clock()
+    frame_rate = 60  # TODO: Choose your own frame rate
+
     game = Game(screen)  # the Model
     viewer = View(screen, game)  # the View
     controller = Controller(game)  # the Controller
 
-    frame_rate = 60  # TODO: Choose your own frame rate
-
     while True:
         clock.tick(frame_rate)
+
         controller.get_and_handle_events()
         game.run_one_cycle()
         viewer.draw_everything()  # Includes the pygame.display.update()
