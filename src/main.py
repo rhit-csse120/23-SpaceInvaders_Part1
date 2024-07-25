a0a639ef7c0042e7a9f199b690c74b0f2d3fe9e7
David Mutchler <mutchler@rose-hulman.edu>
2024-07-25 18:06:59 -0400

    Small improvements.

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from View import View
 def main():
     pygame.init()
     screen = pygame.display.set_mode((640, 650))  # TODO: Choose your own size
-    pygame.display.set_caption("My Project")  # TODO: Choose your own title
+    pygame.display.set_caption("My Game Name")  # TODO: Choose your own title
     clock = pygame.time.Clock()
     frame_rate = 60  # TODO: Choose your own frame rate
 
